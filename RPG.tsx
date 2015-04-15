74b8a2f3ed1c0be2ccad9f7d20dab1f618338c2c
Patrawr <pwawrzoszek@hotmail.com>
2015-04-14 23:55:41 -0400

    Added full tilemapp and things

---
+++ RPG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="RPG" tilewidth="128" tileheight="128">
+ <image source="../Assets/Kenney Pack/Art (13.544 files)/RPG pack (230 assets)/Spritesheet/RPGpack_sheet.png" width="2560" height="1664"/>
+</tileset>
